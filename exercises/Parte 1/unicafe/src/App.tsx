610945ed04c2872ac3711d40d454620c1adcfc0a
Jose Angara <jmeh1992@gmail.com>
2023-06-26 15:38:58 -0600

    Solved exercise 1.8

--- exercises/Parte 1/unicafe/src/App.tsx
+++ exercises/Parte 1/unicafe/src/App.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useState } from "react";
 import FeedbackForm from "./features/FeedbackForm";
 import Button from "./components/Button";
-import Display from "./components/Display";
+import Statistics from "./features/Statistics";
 
 type Statistics = {
   good: number;
@@ -16,29 +16,6 @@ function App() {
     bad: 0,
   });
 
-  const statsValue = {
-    good: 1,
-    neutral: 0,
-    bad: -1,
-  };
-
-  const getTotal = () => stats.good + stats.neutral + stats.bad;
-  const getAverage = () =>
-    (stats.good * statsValue.good +
-      stats.neutral * statsValue.neutral +
-      stats.bad * statsValue.bad) /
-      getTotal() || 0;
-  const getPositive = () => (stats.good / getTotal()) * 100 || 0;
-
-  const statsArray: Array<{ label: string; value: ReactNode }> = [
-    { label: "Good", value: stats.good },
-    { label: "Neutral", value: stats.neutral },
-    { label: "Bad", value: stats.bad },
-    { label: "Total", value: getTotal() },
-    { label: "Average", value: getAverage().toFixed(2) },
-    { label: "Positive", value: `${getPositive().toFixed(2)} %` },
-  ];
-
   const handleGoodFeedback = () => setStats({ ...stats, good: stats.good + 1 });
   const handleNeutralFeedback = () =>
     setStats({ ...stats, neutral: stats.neutral + 1 });
@@ -52,7 +29,7 @@ function App() {
         <Button handleClick={handleBadFeedback}>Bad</Button>
       </FeedbackForm>
 
-      <Display title="Statistics" data={statsArray} />
+      <Statistics stats={stats} />
     </div>
   );
 }
