2ae7ca710aa15a850bf986640c5ff8f71a48b321
Jose Angara <jmeh1992@gmail.com>
2023-06-26 15:30:54 -0600

    Solved exercise 1.7

--- exercises/Parte 1/unicafe/src/App.tsx
+++ exercises/Parte 1/unicafe/src/App.tsx
@@ -16,10 +16,27 @@ function App() {
     bad: 0,
   });
 
+  const statsValue = {
+    good: 1,
+    neutral: 0,
+    bad: -1,
+  };
+
+  const getTotal = () => stats.good + stats.neutral + stats.bad;
+  const getAverage = () =>
+    (stats.good * statsValue.good +
+      stats.neutral * statsValue.neutral +
+      stats.bad * statsValue.bad) /
+      getTotal() || 0;
+  const getPositive = () => (stats.good / getTotal()) * 100 || 0;
+
   const statsArray: Array<{ label: string; value: ReactNode }> = [
     { label: "Good", value: stats.good },
     { label: "Neutral", value: stats.neutral },
     { label: "Bad", value: stats.bad },
+    { label: "Total", value: getTotal() },
+    { label: "Average", value: getAverage().toFixed(2) },
+    { label: "Positive", value: `${getPositive().toFixed(2)} %` },
   ];
 
   const handleGoodFeedback = () => setStats({ ...stats, good: stats.good + 1 });
